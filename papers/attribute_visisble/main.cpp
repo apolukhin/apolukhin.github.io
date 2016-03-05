87a3f8914ee763ea956f252a97211bd092f40060
Antony Polukhin <antoshkka@gmail.com>
2016-03-05 21:25:23 +0300

    Updated the [[visible]] atribute proposal: added notes about nested classes, improved helper programs, more analysis and more polishing

--- papers/attribute_visisble/main.cpp
+++ papers/attribute_visisble/main.cpp
@@ -1,124 +1,125 @@
+// main.cpp
 #define __visible__ __attribute__((visibility("default")))
 
-static __visible__ int cpp_variable_static = 0;
-__visible__ int cpp_variable_noextern = 0;
-__visible__ const int cpp_variable_noextern_const = 0;
+#define NONCLASS_ENTITIES(prefix)                                           \
+    /*variables*/                                                           \
+    static  __visible__ int prefix ## __variable_static = 0;                \
+            __visible__ int prefix ## __variable_noextern = 0;              \
+            __visible__ const int prefix ## __variable_noextern_const = 0;  \
+    extern  __visible__ int prefix ## __variable_extern;                    \
+    int prefix ## __variable_extern = 0;                                    \
+                                                                            \
+    /*functions*/                                                           \
+    __visible__ inline  int prefix ## __function_inline() { return 0; };    \
+    __visible__ static  int prefix ## __function_static() { return 0; };    \
+    __visible__         int prefix ## __function() { return 0; };           \
+    /**/
 
-extern __visible__ int cpp_variable_extern;
-int cpp_variable_extern = 0;
+#define CLASS_MEMBER_ENTITIES(prefix)                                                   \
+    public:                                                                             \
+        static  int prefix ##   __public_static_member_function_inline() { return 0; }  \
+        static  int prefix ##   __public_static_member_function();                      \
+        static  int prefix ##   __public_static_member_variable;                        \
+                int prefix ##   __public_member_function_inline() { return 0; }         \
+        inline  int prefix ##   __public_member_function_inline_not_in_body();          \
+                int prefix ##   __public_member_function();                             \
+                int prefix ##   __public_member_variable;                               \
+    private:                                                                            \
+        static  int prefix ##   __private_static_member_function_inline() { return 0; } \
+        static  int prefix ##   __private_static_member_function();                     \
+        static  int prefix ##   __private_static_member_variable;                       \
+                int prefix ##   __private_member_function_inline() { return 0; }        \
+                int prefix ##   __private_member_function();                            \
+                int prefix ##   __private_member_variable;                              \
+    /**/
 
-__visible__ inline int cpp_function_inline() { return 0; };
-__visible__ static int cpp_function_static() { return 0; };
-__visible__ int cpp_function() { return 0; };
+#define CLASS_MEMBER_ENTITIES_DEFINITION(prefix)                                \
+    int prefix ## __public_static_member_function() { return 0; }               \
+    int prefix ## __public_static_member_variable = 0;                          \
+    int prefix ## __public_member_function() { return 0; }                      \
+    int prefix ## __public_member_function_inline_not_in_body() { return 0; }   \
+    int prefix ## __private_static_member_function() { return 0; }              \
+    int prefix ## __private_static_member_variable = 0;                         \
+    int prefix ## __private_member_function() { return 0; }                     \
+    /**/
 
-
+NONCLASS_ENTITIES(global_ns)
 
 struct base_nonvis_class_impl {
-    static int cpp_base_nonvis_static_member_function_inline(){ return 0; }
-    static int cpp_base_nonvis_static_member_function();
-    static int cpp_base_nonvis_static_member_variable;
-
-    int cpp_base_nonvis_member_function_inline(){ return 0; }
-    int cpp_base_nonvis_member_function();
-    int cpp_base_nonvis_member_variable;
-
     virtual ~base_nonvis_class_impl(){}
-
-private:
-    static int cpp_base_nonvis_private_static_member_function_inline(){ return 0; }
-    static int cpp_base_nonvis_private_static_member_function();
-    static int cpp_base_nonvis_private_static_member_variable;
-
-    int cpp_base_nonvis_private_member_function_inline(){ return 0; }
-    int cpp_base_nonvis_private_member_function();
-    int cpp_base_nonvis_private_member_variable;
+    CLASS_MEMBER_ENTITIES(global_ns__base_nonvis_class)
 };
-int base_nonvis_class_impl::cpp_base_nonvis_static_member_variable = 0;
-int base_nonvis_class_impl::cpp_base_nonvis_static_member_function() { return 0; }
-int base_nonvis_class_impl::cpp_base_nonvis_member_function() { return 0; }
-
-int base_nonvis_class_impl::cpp_base_nonvis_private_static_member_variable = 0;
-int base_nonvis_class_impl::cpp_base_nonvis_private_static_member_function() { return 0; }
-int base_nonvis_class_impl::cpp_base_nonvis_private_member_function() { return 0; }
+CLASS_MEMBER_ENTITIES_DEFINITION(base_nonvis_class_impl::global_ns__base_nonvis_class)
 
 
+struct __visible__ some_class: base_nonvis_class_impl {
+    CLASS_MEMBER_ENTITIES(global_ns__some_class)
 
-struct __visible__ class_impl: base_nonvis_class_impl {
-    static int cpp_static_member_function_inline(){ return 0; }
-    static int cpp_static_member_function();
-    static int cpp_static_member_variable;
+    int foo() {
+        return base_nonvis_class_impl::global_ns__base_nonvis_class__public_member_function();
+    }
+public:
+    struct public_nested_class { 
+        CLASS_MEMBER_ENTITIES(global_ns__some_class__public_nested_class)
+    };
 
-    int cpp_member_function_inline(){ return 0; }
-    int cpp_member_function();
-    int cpp_member_variable;
-
+    struct public_nested_class_not_inside;
 private:
-    static int cpp_private_static_member_function_inline(){ return 0; }
-    static int cpp_private_static_member_function();
-    static int cpp_private_static_member_variable;
-
-    int cpp_private_member_function_inline(){ return 0; }
-    int cpp_private_member_function();
-    int cpp_private_member_variable;
+    struct private_nested_class {
+        CLASS_MEMBER_ENTITIES(global_ns__some_class__private_nested_class)
+    };
 };
-int class_impl::cpp_static_member_variable = 0;
-int class_impl::cpp_static_member_function() { return 0; }
-int class_impl::cpp_member_function() { return 0; }
 
-int class_impl::cpp_private_static_member_variable = 0;
-int class_impl::cpp_private_static_member_function() { return 0; }
-int class_impl::cpp_private_member_function() { return 0; }
-
-
-
-struct derived_nonvis_class_impl: class_impl {
-    static int cpp_derived_nonvis_static_member_function_inline(){ return 0; }
-    static int cpp_derived_nonvis_static_member_function();
-    static int cpp_derived_nonvis_static_member_variable;
-
-    int cpp_derived_nonvis_member_function_inline(){ return 0; }
-    int cpp_derived_nonvis_member_function();
-    int cpp_derived_nonvis_member_variable;
-
-private:
-    static int cpp_derived_nonvis_private_static_member_function_inline(){ return 0; }
-    static int cpp_derived_nonvis_private_static_member_function();
-    static int cpp_derived_nonvis_private_static_member_variable;
-
-    int cpp_derived_nonvis_private_member_function_inline(){ return 0; }
-    int cpp_derived_nonvis_private_member_function();
-    int cpp_derived_nonvis_private_member_variable;
+struct  some_class::public_nested_class_not_inside {
+    CLASS_MEMBER_ENTITIES(global_ns__some_class__public_nested_class_not_inside)
 };
-int derived_nonvis_class_impl::cpp_derived_nonvis_static_member_variable = 0;
-int derived_nonvis_class_impl::cpp_derived_nonvis_static_member_function() { return 0; }
-int derived_nonvis_class_impl::cpp_derived_nonvis_member_function() { return 0; }
 
-int derived_nonvis_class_impl::cpp_derived_nonvis_private_static_member_variable = 0;
-int derived_nonvis_class_impl::cpp_derived_nonvis_private_static_member_function() { return 0; }
-int derived_nonvis_class_impl::cpp_derived_nonvis_private_member_function() { return 0; }
+CLASS_MEMBER_ENTITIES_DEFINITION(some_class::global_ns__some_class)
+CLASS_MEMBER_ENTITIES_DEFINITION(some_class::public_nested_class::global_ns__some_class__public_nested_class)
+CLASS_MEMBER_ENTITIES_DEFINITION(some_class::private_nested_class::global_ns__some_class__private_nested_class)
+CLASS_MEMBER_ENTITIES_DEFINITION(some_class::public_nested_class_not_inside::global_ns__some_class__public_nested_class_not_inside)
+
+
+struct derived_nonvis_class_impl: some_class {
+    CLASS_MEMBER_ENTITIES(global_ns__derived_nonvis_class)
+};
+CLASS_MEMBER_ENTITIES_DEFINITION(derived_nonvis_class_impl::global_ns__derived_nonvis_class)
+
 
 namespace /*anonymous_ns*/ {
-
-static __visible__ int cpp_anonymous_ns_variable_static = 0;
-__visible__ int cpp_anonymous_ns_variable_noextern = 0;
-extern __visible__ int cpp_anonymous_ns_variable_extern;
-__visible__ inline int cpp_anonymous_ns_function_inline() { return 0; };
-__visible__ static int cpp_anonymous_ns_function_static() { return 0; };
-__visible__ int cpp_anonymous_ns_function() { return 0; };
-
-struct __visible__ class_impl {
-    static int cpp_anonymous_ns_static_member_function_inline(){ return 0; }
-    static int cpp_anonymous_ns_static_member_function();
-    static int cpp_anonymous_ns_static_member_variable;
-
-    int cpp_anonymous_ns_member_function_inline(){ return 0; }
-    int cpp_anonymous_ns_member_function();
-    int cpp_anonymous_ns_member_variable;
-};
-
-int cpp_anonymous_ns_variable_extern = 0;
-int class_impl::cpp_anonymous_ns_static_member_variable = 0;
-int class_impl::cpp_anonymous_ns_static_member_function() { return 0; }
-int class_impl::cpp_anonymous_ns_member_function() { return 0; }
-
+    NONCLASS_ENTITIES(anonymous_ns)
+    struct __visible__ anonymous_ns_class {
+        CLASS_MEMBER_ENTITIES(anonymous_ns__some_class)
+    };
+    CLASS_MEMBER_ENTITIES_DEFINITION(anonymous_ns_class::anonymous_ns__some_class)
 }
+
+namespace visible_ns __visible__ {
+    NONCLASS_ENTITIES(visible_ns)
+    struct __visible__ visible_ns_class {
+        CLASS_MEMBER_ENTITIES(visible_ns__some_class)
+    };
+    CLASS_MEMBER_ENTITIES_DEFINITION(visible_ns_class::visible_ns__some_class)
+
+    namespace nested {
+        NONCLASS_ENTITIES(visible_ns__nested_ns)
+        struct __visible__ visible_ns_nested_ns_class {
+            CLASS_MEMBER_ENTITIES(visible_ns__nested_ns__some_class)
+        public:
+            struct public_nested_class { 
+                CLASS_MEMBER_ENTITIES(visible_ns__nested_ns__some_class__public_nested_class)
+            };
+
+            struct public_nested_class_not_inside;
+        };
+
+        struct visible_ns_nested_ns_class::public_nested_class_not_inside {
+                CLASS_MEMBER_ENTITIES(visible_ns__nested_ns__some_class__public_nested_class_not_inside)
+        };
+
+        CLASS_MEMBER_ENTITIES_DEFINITION(visible_ns_nested_ns_class::visible_ns__nested_ns__some_class)
+        CLASS_MEMBER_ENTITIES_DEFINITION(visible_ns_nested_ns_class::public_nested_class::visible_ns__nested_ns__some_class__public_nested_class)
+        CLASS_MEMBER_ENTITIES_DEFINITION(visible_ns_nested_ns_class::public_nested_class_not_inside::visible_ns__nested_ns__some_class__public_nested_class_not_inside)
+    }
+}
+
--- papers/attribute_visisble/match.py
+++ papers/attribute_visisble/match.py
@@ -3,21 +3,32 @@
 import re
 import subprocess
 
-def get_visible(compiler):
-    output = subprocess.Popen(compiler + ' -g -fvisibility=hidden -shared main.cpp -o libmain.so && readelf -sW libmain.so |grep "GLOBAL" | less',
+# Compiles the main.cpp, extracts all the exported entities using readelf
+def get_exported_entities(compiler, all_entities):
+    popen_result = subprocess.Popen(compiler + ' -g -fvisibility=hidden -shared main.cpp -o libmain.so && readelf -sW libmain.so',
         shell=True,
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE).stdout
+        stderr=subprocess.PIPE)
+    popen_result.wait()
 
-    visible = set()
+    if popen_result.returncode != 0:
+        print popen_result.stderr.read()
+        exit(popen_result.returncode)
+
+    output = popen_result.stdout
+    exported = set()
     while True:
-        symbol = output.readline()
-        if not symbol:
+        line = output.readline()
+        if not line:
             break
 
-        visible.update([vis for vis in parsed if vis in symbol])
+        if not " GLOBAL " in line:
+            continue
 
-    return visible
+        exported.update([e for e in all_entities if e in line])
+
+    return exported
+
 
 def extend_with_colors(items):
     ext = {}
@@ -27,78 +38,125 @@ def extend_with_colors(items):
     items.update(ext)
 
 
-parsed = set()
-with open("main.cpp") as f:
-    for line in f:
-        m = re.search("(cpp_[_a-z]+)", line)
-        if m:
-            parsed.add(m.group(1))
+# Preprocesses the main.cpp, extracts all the entities with two underlines in the middle
+def get_all_entities():
+    popen_result = subprocess.Popen('g++ -E main.cpp',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    popen_result.wait()
 
-extern_linkage = set([
-    'cpp_member_function',
-    'cpp_function_inline',
-    'cpp_static_member_function',
-    'cpp_variable_noextern',
-    'cpp_variable_extern',
-    'cpp_member_function_inline',
-    'cpp_function',
-    'cpp_static_member_function_inline',
-    'cpp_static_member_variable',
-])
+    if popen_result.returncode != 0:
+        print popen_result.stderr.read()
+        exit(popen_result.returncode)
 
-more_extern = set()
-for i in extern_linkage:
-    if "member" in i:
-        more_extern.add('cpp_derived_nonvis_' + i[4:])
-        more_extern.add('cpp_base_nonvis_' + i[4:])
-        more_extern.add('cpp_derived_nonvis_private_' + i[4:])
-        more_extern.add('cpp_base_nonvis_private_' + i[4:])
-        more_extern.add('cpp_private_' + i[4:])
+    output = popen_result.stdout
+    all_entities = []
+    while True:
+        line = output.readline()
+        if not line:
+            break
 
-extern_linkage.update(more_extern)
+        all_entities += re.findall("([_a-z]+__[_a-z]+)", line)
 
-gcc46_symbols = get_visible('g++-4.6')
-gcc48_symbols = get_visible('g++-4.8')
-gcc53_symbols = get_visible('g++-5')
-clang34_symbols = get_visible('clang++')
+    return list(set(all_entities))
 
 
-result = []
+def get_extern_linkage_entities():
+    vis_suffix = [
+        '__function',
+        '__function_inline',
+        '__variable_noextern',
+        '__variable_extern',
 
-pattern = (
-    "<tr> "
-        "<td>{name}</td> "
-        "<td bgcolor='{gcc46_color}'>{gcc46}</td> "
-        "<td bgcolor='{gcc48_color}'>{gcc48}</td> "
-        "<td bgcolor='{gcc53_color}'>{gcc53}</td> "
-        "<td bgcolor='{clang34_color}'>{clang34}</td> "
-        "<td bgcolor='{computed_color}'>{computed}</td> "
-        "<td>=</td> "
-        "<td bgcolor='{linkage_color}'>{linkage}</td> "
-        "<td bgcolor='{noinline_color}'>{noinline}</td> "
-        "<td bgcolor='{visible_color}'>{visible}</td> "
-    "</tr>"
-)
+        '__public_member_function',
+        '__public_member_function_inline',
+        '__public_static_member_function',
+        '__public_static_member_function_inline',
 
-for symbol in parsed:
-    not_base_or_derived = not "derived" in symbol and not "base" in symbol
-    is_extern = symbol in extern_linkage and not "anonymous_ns" in symbol
-    not_inline = not "inline" in symbol
-    computed_vis = not_base_or_derived and not_inline and is_extern
-    
-    s = {
-        'name': symbol,
-        'gcc46': symbol in gcc46_symbols,
-        'gcc48': symbol in gcc48_symbols,
-        'gcc53': symbol in gcc53_symbols,
-        'clang34': symbol in clang34_symbols,
-        'computed': computed_vis,
-        'linkage': is_extern,
-        'noinline': not_inline,
-        'visible': not_base_or_derived,
-    }
-    extend_with_colors(s) 
-    result.append( pattern.format(**s) )    
+        '__private_member_function',
+        '__private_member_function_inline',
+        '__private_static_member_function',
+        '__private_static_member_function_inline',
 
-for l in sorted(result):
-    print l
+        '__public_static_member_variable',
+        '__private_static_member_variable',
+    ]
+
+    extern_linkage = set()
+    for i in vis_suffix:
+        if "member" in i:
+            extern_linkage.add('global_ns__base_nonvis_class' + i)
+            extern_linkage.add('global_ns__some_class' + i)
+            extern_linkage.add('global_ns__derived_nonvis_class' + i)
+            extern_linkage.add('global_ns__some_class__public_nested_class' + i)
+            extern_linkage.add('global_ns__some_class__public_nested_class_not_inside' + i)
+            extern_linkage.add('global_ns__some_class__private_nested_class' + i)
+            extern_linkage.add('visible_ns__some_class' + i)
+            extern_linkage.add('visible_ns__nested_ns__some_class' + i)
+            extern_linkage.add('visible_ns__nested_ns__some_class__public_nested_class' + i)
+            extern_linkage.add('visible_ns__nested_ns__some_class__public_nested_class_not_inside' + i)
+        else:
+            extern_linkage.add('global_ns' + i)
+            extern_linkage.add('visible_ns' + i)
+            extern_linkage.add('visible_ns__nested_ns' + i)
+
+    return extern_linkage
+
+
+
+def main():
+    all_entities = get_all_entities()
+    extern_linkage = get_extern_linkage_entities()
+
+    gcc46_exports = get_exported_entities('g++-4.6', all_entities)
+    gcc48_exports = get_exported_entities('g++-4.8', all_entities)
+    gcc53_exports = get_exported_entities('g++-5', all_entities)
+    clang34_exports = get_exported_entities('clang++', all_entities)
+
+    result = []
+
+    pattern = (
+        "<tr> "
+            "<td>{name}</td> "
+            "<td bgcolor='{gcc46_color}'>{gcc46}</td> "
+            "<td bgcolor='{gcc48_color}'>{gcc48}</td> "
+            "<td bgcolor='{gcc53_color}'>{gcc53}</td> "
+            "<td bgcolor='{clang34_color}'>{clang34}</td> "
+            "<td style='border:none'> </td> "
+            "<td bgcolor='{computed_color}'>{computed}</td> "
+            "<td>=</td> "
+            "<td bgcolor='{linkage_color}'>{linkage}</td> "
+            "<td>&amp;&amp;</td> "
+            "<td bgcolor='{noinline_color}'>{noinline}</td> "
+            "<td>&amp;&amp;</td> "
+            "<td bgcolor='{visible_color}'>{visible}</td> "
+        "</tr>"
+    )
+
+    for entity in all_entities:
+        not_base_or_derived = not "derived" in entity and not "base" in entity
+        is_extern = entity in extern_linkage
+        not_inline = not "inline" in entity
+        computed_vis = not_base_or_derived and not_inline and is_extern
+        
+        s = {
+            'name': entity,
+            'gcc46': entity in gcc46_exports,
+            'gcc48': entity in gcc48_exports,
+            'gcc53': entity in gcc53_exports,
+            'clang34': entity in clang34_exports,
+            'computed': computed_vis,
+            'linkage': is_extern,
+            'noinline': not_inline,
+            'visible': not_base_or_derived,
+        }
+        extend_with_colors(s) 
+        result.append( pattern.format(**s) )
+
+    for l in sorted(result):
+        print l.replace('__','::').replace('global_ns', '').replace('anonymous_ns', '&lt;anonymous&gt;')
+
+
+if __name__ == "__main__":
+    main()
