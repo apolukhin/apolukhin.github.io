e3dba0d927edd7f5aa869be6703e813c55c7ff65
Antony Polukhin <antoshkka@gmail.com>
2017-12-07 23:01:14 +0300

    Site reworked

--- html_assets/generate_status_table.sh
+++
@@ -1,4 +0,0 @@
-# Script to generate auto-updatable status table for all the libraries I maintain.
-
-find . -name "README.md" -print0 | xargs -0 grep '\[!\[' | grep 'apolukhin' | sed -n 's:./\(.*\)/README.md\:\(.*\):<tr><td>\u\1 \2</td></tr>:gp' | sed 's:|:</td><td>:g' | sed 's:!\[\([a-zA-Z ]*\)\](\([^)]*\)):<img src="\2" alt="\1">:g' | sed 's:\[\([^]]*\)\](\([^)]*\)):<a href="\2">\1</a>:g' | sort
-
